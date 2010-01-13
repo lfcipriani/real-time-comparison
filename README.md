7c73dd70dbffb876e20dfeab3a95bd45b19aabae
Luis Cipriani <lfcipriani@talleye.com>
2010-01-13 15:06:09 +0000

    changing the UI and algorithym to allow the user to select 2 terms to compare

--- README.md
+++ README.md
@@ -1,23 +1,20 @@
 This is a simple example on how to use XMPP, Strophejs and the Collecta API to access their Pubsub search nodes.
 
-Setup
-=====
+# Setup
 
-- Request an Collecta API key: http://developer.collecta.com/KeyRequest/
+- Request an Collecta API key: <http://developer.collecta.com/KeyRequest/>
 - Copy config.js.sample to config.js and set your API key on the API_KEY field
 - Put all these files on any web server
 
-Use
-===
+# Use
 
 - Under construction
 
-Links
-=====
+# Links
 
-- XMPP: http://xmpp.org
-- Strophe: http://code.stanziq.com/strophe/
-- Collecta developer info: http://developer.collecta.com/
-- XMPP Pubsub spec: http://xmpp.org/extensions/xep-0060.html
+- XMPP: <http://xmpp.org>
+- Strophe: <http://code.stanziq.com/strophe/>
+- Collecta developer info: <http://developer.collecta.com/>
+- XMPP Pubsub spec: <http://xmpp.org/extensions/xep-0060.html>
 
 2010 - Luis Cipriani
--- real-time-comparison.js
+++ real-time-comparison.js
@@ -1,5 +1,37 @@
 var connection = null;
-var anon_jid = null;
+var anonymous_jid = null;
+var is_connected = false;
+var terms = [];
+
+var Collecta = {
+	subscribeSearchStanza: function(searchName) {
+		return $iq({type: 'set', from: anonymous_jid, to: 'search.collecta.com', id: searchName })
+			    	.c('pubsub', {xmlns: 'http://jabber.org/protocol/pubsub'})
+			    		.c('subscribe', {node: 'search', jid: anonymous_jid})
+					.up().c('options')
+							.c('x', {xmlns: 'jabber:x:data', type: 'submit'})
+								.c('field', {"var": 'FORM_TYPE', type: 'hidden'})
+									.c('value').t('http://jabber.org/protocol/pubsub#subscribe_options')
+			    				.up().up().c('field', {"var": 'x-collecta#apikey'})
+									.c('value').t(Config.API_KEY)
+								.up().up().c('field', {"var": 'x-collecta#query'})
+									.c('value').t(searchName);
+	},
+	unsubscribeStanza: function() {
+		return $iq({type: 'set', from: anonymous_jid, to: 'search.collecta.com', id: 'unsubscribe42'})
+					.c('pubsub', {xmlns: 'http://jabber.org/protocol/pubsub'})
+				    	.c('unsubscribe', {node: 'search', jid: anonymous_jid});
+	},
+	processItem: function(item) {
+		
+	}
+};
+
+function startComparison() {
+	console.log("Subscribing to nodes: "+ terms[0] +" and "+ terms[1]);
+	connection.send(Collecta.subscribeSearchStanza(terms[0]).tree());
+	connection.send(Collecta.subscribeSearchStanza(terms[1]).tree());
+}
 
 function log(msg) 
 {
@@ -15,6 +47,8 @@ function rawOutput(data)
 {
     log('SENT: ' + data);
 }
+
+// ********* XMPP Callbacks *********
 
 function onConnect(status)
 {
@@ -32,84 +66,56 @@ function onConnect(status)
 		connection.addHandler(onPresence, null, 'presence', null, null, null);                       
 	    connection.addHandler(onIq, null, 'iq', null, null, null); 
 	    connection.addHandler(onMessage, null, 'message', null, null, null);
-	    console.log("sending presence...");
+	    console.log("Sending presence...");
 	    connection.send($pres().tree());
     }
 }
 
 function onPresence(prs) {
-	console.log(prs);
-    anon_jid = $(prs).attr('to');    
+	console.log("Got presence!");
+    anonymous_jid = $(prs).attr('to');
+	$('#go').removeAttr('disabled');
+	$('#warning').text("Type 2 things to compare and go...");
+	return true;
 }
 
 function onIq(xml) {
-    console.log(xml);
-    
-    if ($(xml).find('subscription:first').attr('subscription') == 'subscribed') {
-		console.log("yeah! just wait for new entries...");
-    }
+	xmlDom = $(xml);	
+	var iqId = xmlDom.attr('id');
+	if (iqId == terms[0] || iqId == terms[1]) {
+		if (xmlDom.find('subscription:first').attr('subscription') == 'subscribed') {
+			console.log("Subscribed to "+iqId);
+	    }
+	}
+	return true;
 }
 
 function onMessage(msg) {
     console.log(msg);
+	return true;
 }
 
+// ****************
+
 $(document).ready(function () {
+	$('#go').attr('disabled','disabled');
     connection = new Strophe.Connection(Config.BOSH_SERVICE);
     connection.rawInput = rawInput;
     connection.rawOutput = rawOutput;
+	connection.connect(Config.HOST, null, onConnect);
 
-    $('#connect').bind('click', function () {
-	var button = $('#connect').get(0);
-	if (button.value == 'connect') {
-	    button.value = 'disconnect';
-
-	    connection.connect(Config.HOST, null, onConnect);
-	} else {
-	    button.value = 'connect';
-	    connection.disconnect();
-	}
+    $('#go').bind('click', function () {
+		var button = $('#go').get(0);
+		if (button.value == 'go') {
+		    button.value = 'stop';
+			terms.push($('#term1').val());
+			terms.push($('#term2').val());
+		    startComparison();
+		} else {
+		    button.value = 'go';
+			console.log("Unsubscribing...");
+			connection.send(Collecta.unsubscribeStanza().tree());
+		    //connection.disconnect();
+		}
     });
-
-	$('#unsubfh').bind('click', function () {
-		var unsubscribe = $iq({type: 'set', from: anon_jid, to: 'search.collecta.com', id: 'unsubscribe42'})
-				    	.c('pubsub', {xmlns: 'http://jabber.org/protocol/pubsub'})
-				    		.c('unsubscribe', {node: 'search', jid: anon_jid});
-	    connection.send(unsubscribe.tree());
-    });
-
-	$('#subasket').bind('click', function () {
-		var subscribe = $iq({type: 'set', from: anon_jid, to: 'search.collecta.com', id: 'subscribe42'})
-	    	.c('pubsub', {xmlns: 'http://jabber.org/protocol/pubsub'})
-	    		.c('subscribe', {node: 'search', jid: anon_jid})
-			.up().c('options')
-					.c('x', {xmlns: 'jabber:x:data', type: 'submit'})
-						.c('field', {"var": 'FORM_TYPE', type: 'hidden'})
-							.c('value').t('http://jabber.org/protocol/pubsub#subscribe_options')
-	    				.up().up().c('field', {"var": 'x-collecta#apikey'})
-							.c('value').t(Config.API_KEY)
-						.up().up().c('field', {"var": 'x-collecta#query'})
-							.c('value').t('basketball');
-
-	    console.log("creating subscription: "+subscribe);
-	    connection.send(subscribe.tree());
-    });
-
-	$('#subsoccer').bind('click', function () {
-		var subscribe = $iq({type: 'set', from: anon_jid, to: 'search.collecta.com', id: 'subscribe42'})
-	    	.c('pubsub', {xmlns: 'http://jabber.org/protocol/pubsub'})
-	    		.c('subscribe', {node: 'search', jid: anon_jid})
-			.up().c('options')
-					.c('x', {xmlns: 'jabber:x:data', type: 'submit'})
-						.c('field', {"var": 'FORM_TYPE', type: 'hidden'})
-							.c('value').t('http://jabber.org/protocol/pubsub#subscribe_options')
-	    				.up().up().c('field', {"var": 'x-collecta#apikey'})
-							.c('value').t(Config.API_KEY)
-						.up().up().c('field', {"var": 'x-collecta#query'})
-							.c('value').t('hockey');
-
-	    console.log("creating subscription: "+subscribe);
-	    connection.send(subscribe.tree());
-    });
-
 });
